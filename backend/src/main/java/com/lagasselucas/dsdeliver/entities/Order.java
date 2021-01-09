125a1a56336f37b4ed7632ea14b766051cb5c9a1
Lucas Lagasse <lucas.lagasse@gmail.com>
2021-01-08 23:28:04 -0300

    Order Entities

--- backend/src/main/java/com/lagasselucas/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/lagasselucas/dsdeliver/entities/Order.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Order implements Serializable{
+
+	private static final long serialVersionUID = 1L;
 	
 	private Long id;
 	private String address;
