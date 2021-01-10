a3b25c9aa5ebe9d278fc479cbbbb3e3c12fb9d2e
Lucas Lagasse <lucas.lagasse@gmail.com>
2021-01-09 23:59:48 -0300

    Routes and StepsHeader

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,13 @@
+import StepsHeader from './StepsHeader';
 import './styles.css';
 
 function Orders(){
-    return (
-        <div>
-            <h1>Orders</h1>
+    return(
+        <div className="orders-container">
+            <StepsHeader />
         </div>
     )
+
 }
 
 export default Orders;
