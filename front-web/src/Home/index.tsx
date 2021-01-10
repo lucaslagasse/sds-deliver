037de5818f729d8daa236f86646f36d388fd60b9
Lucas Lagasse <lucas.lagasse@gmail.com>
2021-01-09 22:33:38 -0300

    Add Footer

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,5 +1,6 @@
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg';
+import Footer from '../Footer';
 
 function Home() {
     return (
@@ -25,6 +26,7 @@ function Home() {
                     </div>
                 </div>
             </div>
+            <Footer />
         </>
     )
 }
